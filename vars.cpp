19037d614dd66cf365ea68521ca7dd9e12cbe763
yatomikazuchi <caioevilabner@gmail.com>
2024-05-03 21:06:02 -0300

    updated

---
+++ vars.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //int 
+    1,2,4,6;
+    //float 
+    5.32f,62.34f,12.2f,2.5f,1.0f; //(até 7 digitos);
+    //double 
+    5.32,62.34,12.2,2.5,1.0; //(até 15 digitos);
+
+    //boolean - bool
+    true; false;
+
+    //character - char
+    'a', 'b', '6', 'L';
+    // (')
+
+    int h;
+    h = 23;
+
+    cout << h << endl;
+}
