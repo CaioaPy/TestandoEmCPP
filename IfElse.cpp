b6051204ab2cb3f2ee41c16c2edf17e1b093abbc
yatomikazuchi <caioevilabner@gmail.com>
2024-05-05 10:57:21 -0300

    updated

--- IfElse.cpp
+++ IfElse.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // not can also be as '!'(negation), and as '&&'(all true = true, one false = false), or as '||'(if one true = true, if all false = false)
 
 int main(){
+/*
     bool alive = true;
     bool tired = false;
     if (alive){
@@ -22,4 +23,18 @@ int main(){
             cout << "and resting.." << endl;
         }
     }
+}
+*/
+int money = 674;
+if (money >= 1000){
+    cout << "you saved lots of money!" << endl;
+} else if (800 <= money < 1000){
+    cout << "you can live pretty well" << endl;
+} else if (500 <= money < 800){
+    cout << "you should sell your house" << endl;
+} else if (250 <= money < 500){
+    cout << "thats really bad.." << endl; 
+} else if (0 <= money < 250){
+    cout << "you just cant live with no money" << endl;
+}
 }
