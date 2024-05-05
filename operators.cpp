bacfb0b23780ca2f26d4f6dabd047707a9a0fc7b
yatomikazuchi <caioevilabner@gmail.com>
2024-05-05 10:27:43 -0300

    updated

--- operators.cpp
+++ operators.cpp
@@ -13,4 +13,11 @@ int main() {
     cout << "float (without using (float)): " << w << endl;
     float r = (float) y / a; // when (float) is used, y(5) is gonna get treated as a float, so 5/2 > 2.5 > float > 2.5 stored
     cout << "float (when using (float)): " << r << endl;
+    // ++ and --
+    cout << "inicial value of x: " << x << endl;
+    cout << "using x++: " << x++ << endl;
+    cout << "x: " << x << endl;
+    cout << "now using ++x: " << ++x << endl;
+    cout << "the code is gonna read from left to right so if you print x++ its gonna to be printed as x and after that ++ gonna be readed and executed"
+
 }
