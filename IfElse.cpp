eb3cdae39830af315286a5e49f2a84dba30acf44
yatomikazuchi <caioevilabner@gmail.com>
2024-05-05 10:40:10 -0300

    updated

--- IfElse.cpp
+++ IfElse.cpp
@@ -2,12 +2,14 @@
 #include <math.h>
 using namespace std;
 
+// not can also be as '!'(negation), and as '&&'(all true = true, one false = false), or as '||'(if one true = true, if all false = false)
+
 int main(){
     bool alive = true;
     bool tired = false;
     if (alive){
         cout << "youre alive :)" << endl;
-        if (not tired){
+        if (!tired){
             cout << "and full of energy !!" << endl;
         }
         else{
@@ -16,7 +18,7 @@ int main(){
     }
     else{
         cout << "youre dead :(" << endl;
-        if (not tired){
+        if (!tired){
             cout << "and resting.." << endl;
         }
     }
