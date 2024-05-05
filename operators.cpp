f4b538c2576cf653c383439f28c3387f5243e51f
yatomikazuchi <caioevilabner@gmail.com>
2024-05-04 21:28:06 -0300

    updated

--- operators.cpp
+++ operators.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main() {
-    int x = 20, y = 5, z = 10;
+    int x = 20, y = 5, z = 10, a = 2, b = 3;
     cout << x + y << endl;
     cout << x - z << endl;
     cout << x / y << endl;
     cout << z * y << endl;
+    cout << pow(2,3) << endl;
 }
