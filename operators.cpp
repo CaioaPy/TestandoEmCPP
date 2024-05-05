e70fb1667cd20bb2e538a6641c78541670c80a1a
yatomikazuchi <caioevilabner@gmail.com>
2024-05-04 21:25:16 -0300

    updated

---
+++ operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+#include <math.h>;
+using namespace std;
+
+int main() {
+    int x = 20, y = 5, z = 10;
+    cout << x + y << endl;
+    cout << x - z << endl;
+    cout << x / y << endl;
+    cout << z * y << endl;
+}
