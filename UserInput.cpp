336f2fd050a3970a18ea8721497fb30e77c32f49
yatomikazuchi <caioevilabner@gmail.com>
2024-05-06 20:20:51 -0300

    updated

--- UserInput.cpp
+++ UserInput.cpp
@@ -7,9 +7,12 @@ int main(){
     string name = "weeza";
     int age = 23;
     char sex = 'N';
-    cout << "name: " << name << ", age: " << age << ", sex: " << sex << endl;
+    //cout << "name: " << name << ", age: " << age << ", sex: " << sex << endl;
+    cout << "insert your name: ";
     cin >> name;
+    cout << "insert your age: ";
     cin >> age;
+    cout << "insert your sex: ";
     cin >> sex;
     cout << "name: " << name << ", age: " << age << ", sex: " << sex << endl;
 
