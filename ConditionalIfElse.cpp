620b4ca955e0497e2cc93d7d7c451cf841b01ff9
yatomikazuchi <caioevilabner@gmail.com>
2024-05-06 18:56:51 -0300

    updated

--- ConditionalIfElse.cpp
+++ ConditionalIfElse.cpp
@@ -3,20 +3,25 @@
 using namespace std;
 
 int main(){
-    int rating = 85;
+    int rating = 60;
     char letter;
+    bool active = true;
     /*
     if (rating >= 70) {
         letter = 'A';
     }
     else {
-        letter = 'F';
+        if (active)
+            letter = 'B';
+        else
+            letter = 'F';
     }
     */
     /*
     if (rating >= 70) letter = 'A';
+    else if (rating < 70, active) letter = 'B';
     else letter = 'F';
     */
-    letter = (rating >= 70) ? 'A' : 'F';
+    letter = (rating >= 70) ? 'A' : (active) ? 'B' : 'F';
     cout << "Rating: " << letter << endl;
 }
