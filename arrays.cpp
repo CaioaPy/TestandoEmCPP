6feab05e99fc655d918e9812b64278661ba7abd7
yatomikazuchi <caioevilabner@gmail.com>
2024-05-07 18:58:54 -0300

    updated

--- arrays.cpp
+++ arrays.cpp
@@ -6,10 +6,13 @@ int main(){
     int entry1 = 1;
     int entry2 = 2;
     int entry3 = 4;
+    /*
     int entries[3];
     entries[0] = 1;
     entries[1] = 2;
     entries[2] = 4;
+    */
+    int entries[] = {1, 2, 4};
     cout << entries[0] << endl;
     cout << entries[1] << endl;
     cout << entries[2] << endl;
