e0d6365af4e03f9f7d04ae3f736d84ffc7b37aab
yatomikazuchi <caioevilabner@gmail.com>
2024-05-07 18:56:05 -0300

    updated

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int entry1 = 1;
+    int entry2 = 2;
+    int entry3 = 4;
+    int entries[3];
+    entries[0] = 1;
+    entries[1] = 2;
+    entries[2] = 4;
+    cout << entries[0] << endl;
+    cout << entries[1] << endl;
+    cout << entries[2] << endl;
+}
