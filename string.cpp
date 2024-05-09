5a3f8a4bdc084cf4c4fbb37f2a99a63b5e7bae39
yatomikazuchi <caioevilabner@gmail.com>
2024-05-09 18:25:25 -0300

    updated

--- string.cpp
+++ string.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main(){
     //string = array of characters
 
-    //string name = "Flake";
-    string name = {'F', 'l', 'a', 'k', 'e'};
+    string name = "Flake";
+    //string name = {'F', 'l', 'a', 'k', 'e'};
     cout << name << endl;
-
+    cout << name.length() << endl;
+    cout << (name.length() == 0) << endl; //checking if the var is blank
+    cout << (name == "") << endl; // another method for check
 }
