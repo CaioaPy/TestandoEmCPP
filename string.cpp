5aca785e10a50d2bbf2b3aa8fe3455d731f53adf
yatomikazuchi <caioevilabner@gmail.com>
2024-05-08 23:02:33 -0300

    updated

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    //string = array of characters
+
+    //string name = "Flake";
+    string name = {'F', 'l', 'a', 'k', 'e'};
+    cout << name << endl;
+
+}
