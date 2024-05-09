015a18e248fcd0543c24347f7532d3ef3ed3f179
yatomikazuchi <caioevilabner@gmail.com>
2024-05-09 18:34:35 -0300

    updated

--- string.cpp
+++ string.cpp
@@ -9,8 +9,23 @@ int main(){
     string name = "Flake";
     //string name = {'F', 'l', 'a', 'k', 'e'};
     cout << name << endl;
-    cout << name.length() << endl;
+    
+    /*
+    cout << name.length() << endl; // check and print the number of elements(Chars) in a string
     cout << (name.length() == 0) << endl; //checking if the var is blank
     cout << (name == "") << endl; // another method for check
     cout << name.empty() << endl; // another method for check
+    */
+    name += '!';
+    name += " woo";
+    cout << name << endl;
+
+    name[2] = 'Y';
+    cout << name << endl;
+
+    name = "C++" + name; // this is the same as name += "C++"
+    cout << name << endl;
+
+    name.append(3, '.');
+    cout << name << endl;
 }
