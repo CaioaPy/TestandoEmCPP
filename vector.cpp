99a84000b27eff46dcdbd0bd0f55d8e8b0417bf5
yatomikazuchi <caioevilabner@gmail.com>
2024-05-10 20:06:26 -0300

    updated

--- vector.cpp
+++ vector.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-    vector<int> tokens;
+    // vector<int> tokens;
+    vector<int> tokens({100, 232, 1236}); //you can start a vector with values like arrays
     
     tokens.push_back(10); //push_back function adds the element at the end of the vector
-    tokens.push_back(132);
     tokens.push_back(1512);
 
-    tokens[0] = 162; // you can also change a specifc index value
+    tokens[4] = 162; // you can also change a specifc index value
 
     cout << tokens.size() << endl;
     // cout << (tokens.size() == 0) << endl;
@@ -18,5 +18,7 @@ int main(){
     cout << tokens[0] << endl;
     cout << tokens[1] << endl;
     cout << tokens[2] << endl;
+    cout << tokens[3] << endl;
+    cout << tokens[4] << endl;
 
 }
