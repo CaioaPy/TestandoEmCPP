067f689659a899f7352f289f00345f8ba4a0213d
yatomikazuchi <caioevilabner@gmail.com>
2024-05-11 15:49:22 -0300

    updated

--- ForLoops.cpp
+++ ForLoops.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main() {
     int water = 0;
     
-    for (int i = 0; i < 10; i++){
-        cout << "water coming!! ";
+    for (int i = 0; i < 11; i++){
+        cout << "water coming! ";
         water++;
         cout << "water = " << water << endl;
     }
+
+    for (int i = 3; i > 0; i--){
+        cout << i << "! ";
+    }
+    cout << endl; 
+    cout << water << " waters!" << endl;
 }
