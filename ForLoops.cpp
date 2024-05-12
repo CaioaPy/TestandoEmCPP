2d2ac8083662dc6cd8266e5c55ec0cc40cb87cde
yatomikazuchi <caioevilabner@gmail.com>
2024-05-12 12:31:36 -0300

    updated

--- ForLoops.cpp
+++ ForLoops.cpp
@@ -31,11 +31,12 @@ int main() {
     int x = factorial(3, 1);
     cout << "3! = " << x << endl;
     */
-    cout << "befor:" << endl;
+    cout << "before:" << endl;
     for (size_t i = 0; i < houses.size(); i++){
         cout << houses[i] << " ";
     }
     cout << endl;
+    cout << "using (V : Vs) method" << endl;
     for (int house : houses){
         house += 6;
     }
@@ -44,8 +45,21 @@ int main() {
         cout << houses[i] << " ";
     }
     cout << endl;
-    cout << "when using for loops you modify the temp var not the vector"
-    
+    cout << "when using for loops you modify the temp var not the vector" << endl;
+
+    cout << "before:" << endl;
+    for (size_t i = 0; i < houses.size(); i++){
+        cout << houses[i] << " ";
+    }
+    cout << endl;
+    cout << "now using index method" << endl;
+    for (size_t i = 0; i < houses.size(); i++){
+        houses[i] += 6;
+    }
+    cout << "after:" << endl;
+    for (size_t i = 0; i < houses.size(); i++){
+        cout << houses[i] << " ";
+    }
     /*
     for (int house : houses){
         cout << house << " ";
