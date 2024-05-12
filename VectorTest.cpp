471e8b32ef3c57d98db73d113546a7a81a203236
yatomikazuchi <caioevilabner@gmail.com>
2024-05-12 12:51:59 -0300

    updated

--- VectorTest.cpp
+++ VectorTest.cpp
@@ -5,18 +5,36 @@ using namespace std;
 
 int main() {
     int x;
+    char decision;
+    bool continuer = true;
     vector<int> Values;
-    cout << "Value to add to the list: " << endl;
-    cin >> x;
-    Values.insert(Values.end(), x);
-    cout << "Value to add to the list: " << endl;
-    cin >> x;
-    Values.insert(Values.end(), x);
-    cout << "Value to add to the list: " << endl;
-    cin >> x;
-    Values.insert(Values.end(), x);
-    for (int Value : Values) {
-        cout << Value << " ";
-    }
-    cout << endl;
+    do {
+        cout << "What do you want to do? add value (A), see the list (S), exit and see(E)" << endl;
+        cin >> decision;
+        switch(decision) {
+            case('A'):
+                cout << "Value to add to the list: " << endl;
+                cin >> x;
+                Values.insert(Values.end(), x);
+                break;
+
+            case('S'):
+                for (int Value : Values) {
+                    cout << Value << " ";
+                }
+                cout << endl;
+                break;
+
+            case('E'):
+                continuer = false;
+                for (int Value : Values) {
+                    cout << Value << " ";
+                }
+                cout << endl;
+                break;
+
+            default:
+                cout << "invalid" << endl;
+        }
+    } while (continuer == true);
 }
