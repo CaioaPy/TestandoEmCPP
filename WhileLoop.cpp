fccc20b690426c65b53f9ff9e254ed58857178b1
yatomikazuchi <caioevilabner@gmail.com>
2024-05-14 19:34:21 -0300

    updated

--- WhileLoop.cpp
+++ WhileLoop.cpp
@@ -7,9 +7,25 @@ using namespace std;
 int main() {
     vector<string> students = {"Jeff", "Roger", "Romeo", "Stark", "Claire", "Amy", "Rose", "Paul"};
     string target;
+    string x;
+    int i = 0;
     bool found = false;
 
-    while (found == false) {
-        cout << "a" << endl;
+    cout << "insert the target name: " << endl;
+    cin >> target;
+    while (found == false && i < students.size()) {
+        x = students[i];
+        if (x == target) {
+            cout << x << " found!" << endl;
+            cout << x << " is a student." << endl;
+            found = true;
+        }
+        else if (x != target) {
+            cout << x << endl;
+        }
+        i++;
+    }
+    if (found == false) {
+        cout << target << " wasn't found!";
     }
 }
