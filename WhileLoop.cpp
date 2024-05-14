e63b974b4b4fc04d33cd4315add78220b654b7e5
yatomikazuchi <caioevilabner@gmail.com>
2024-05-14 19:22:10 -0300

    updated

---
+++ WhileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    vector<string> students = {"Jeff", "Roger", "Romeo", "Stark", "Claire", "Amy", "Rose", "Paul"};
+    string target;
+    bool found = false;
+
+    while (found == false) {
+        cout << "a" << endl;
+    }
+}
