e8e9efbe3358ae8abd4197307592851217ca1b5d
yatomikazuchi <caioevilabner@gmail.com>
2024-05-15 22:47:41 -0300

    updated

--- random.cpp
+++ random.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() { 
-    int x, y;
-    x = rand() % 10;
-    y = rand() % 100;
+    int x, y, z;
+    x = rand() % 10; // 0 - 9
+    y = rand() % 100; // 0 - 99
+    z = rand() % 3 + 10; // 10 - 12(0 - 2 + 10) 
     cout << x << endl;
     cout << y << endl;
+    cout << z << endl;
 }
