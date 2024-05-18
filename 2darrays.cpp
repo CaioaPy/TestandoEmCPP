847676cf1442652af7726f6ddd4b66dfca0c3377
yatomikazuchi <caioevilabner@gmail.com>
2024-05-17 22:11:17 -0300

    updated

--- 2darrays.cpp
+++ 2darrays.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int main() {
 
+    //using arrays
+    /*
     string moedas[5][3] = { {"EUA", "DOL", "Dolar"}, 
                             {"Canada", "CAD", "Dolar"}, 
                             {"Brasil", "BRL", "Real"}, 
@@ -16,6 +18,22 @@ int main() {
         for (int j = 0; j < 3; j++){
             cout << moedas[i][j] << " ";
         } 
+        cout << endl;
+    }
+    */
+
+    //using vector
+    vector<vector<string>> Estoque = {
+                                        {"Agua", "Refri", "Cafe"},
+                                        {"Miojo"},
+                                        {"Boneco", "Carrinho"},
+                                        {"Carne", "Peixe"}
+                                    };
+    Estoque[2].push_back("Aviaozinho");
+    for (size_t i = 0; i < Estoque.size(); i++){
+        for (size_t j = 0; j < Estoque[i].size(); j++){
+            cout << Estoque[i][j] << " ";
+        }
         cout << endl;
     }
 }
