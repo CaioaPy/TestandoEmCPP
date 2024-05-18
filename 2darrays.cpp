48479e5f6699eb4b9154016308e930254fd9cec8
yatomikazuchi <caioevilabner@gmail.com>
2024-05-17 21:53:45 -0300

    updated

---
+++ 2darrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    string moedas[5][3] = { {"EUA", "DOL", "Dolar"}, 
+                            {"Canada", "CAD", "Dolar"}, 
+                            {"Brasil", "BRL", "Real"}, 
+                            {"RU", "GBP", "Libra"}, 
+                            {"Espanha", "EUR", "Euro"}
+                        };
+    cout << moedas[0][0]; << " "
+    cout << moedas[0][1]; << " "
+    cout << moedas[0][2]; 
+
+}
