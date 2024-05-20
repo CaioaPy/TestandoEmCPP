ae8549eba41d118b8e3f25009164bc5f404ca661
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-20 12:37:45 -0300

    Update TicTacToe.cpp

--- Projects/TicTacToe.cpp
+++ Projects/TicTacToe.cpp
@@ -11,6 +11,7 @@ int main() {
                             {" 7 ", "|", " 8 ", "|", " 9 "}
                         };
     int dec;
+    bool playing = false;
     cout << "each slot has a number that is assigned to himself" << endl;
     cout << "you're gonna use them to play, they are assigned like that:" << endl;
     for (int i = 0; i < 5; i++){
@@ -18,35 +19,53 @@ int main() {
             cout << Board[i][j] << " ";
         } cout << endl;
     }
-    cout << "you're going to play as X, so where do you want to start?" << endl;
-    cin >> dec;
-    switch(dec){
-        case(1):
-            cout << " a " << endl;
-            break;
-        case(2):
-            cout << " a " << endl;
-            break;
-        case(3):
-            cout << " a " << endl;
-            break;
-        case(4):
-            cout << " a " << endl;
-            break;
-        case(5):
-            cout << " a " << endl;
-            break;
-        case(6):
-            cout << " a " << endl;
-            break;
-        case(7):
-            cout << " a " << endl;
-            break;
-        case(8):
-            cout << " a " << endl;
-            break;
-        case(9):
-            cout << " a " << endl;
-            break;
-    }
+    cout << "you're going to play as X" << endl;
+    cout << "\n\n\n\n\n";
+    playing = true;
+    do {
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                cout << Board[i][j] << " ";
+            } cout << endl;
+        }
+        cout << "your turn!" << endl;
+        cin >> dec;
+        switch(dec){
+            case(1):
+                if (Board[0][0] == " 1 ") {
+                    Board[0][0] = " X ";
+                    break;
+                }
+                else
+                    cout << "invalid move" << endl;
+                    break;
+            case(2):
+                cout << " a " << endl;
+                break;
+            case(3):
+                cout << " a " << endl;
+                break;
+            case(4):
+                cout << " a " << endl;
+                break;
+            case(5):
+                cout << " a " << endl;
+                break;
+            case(6):
+                cout << " a " << endl;
+                break;
+            case(7):
+                cout << " a " << endl;
+                break;
+            case(8):
+                cout << " a " << endl;
+                break;
+            case(9):
+                cout << " a " << endl;
+                break;
+            default:
+                cout << "error, try again" << endl;
+                break;
+        }
+    } while (playing);
 }
