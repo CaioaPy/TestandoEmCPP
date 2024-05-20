2d855d4b103ec729e3eb9a5dd4beec32e6040b85
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-20 12:18:28 -0300

    Update TicTacToe.cpp

--- Projects/TicTacToe.cpp
+++ Projects/TicTacToe.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string Board [5][5] = { {"   ", "|", "   ", "|", "   "},
+                            {"---", "-", "---", "-", "---"},
+                            {"   ", "|", "   ", "|", "   "},
+                            {"---", "-", "---", "-", "---"},
+                            {"   ", "|", "   ", "|", "   "}
+                        };
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            cout << Board[i][j] << " ";
+        } cout << endl;
+    } 
+}
