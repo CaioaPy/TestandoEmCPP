70895765ca05992718bc764d552e839109edc4a8
yatomikazuchi <caioevilabner@gmail.com>
2024-05-21 20:36:02 -0300

    updated

--- Projects/TicTacToe.cpp
+++ Projects/TicTacToe.cpp
@@ -13,6 +13,16 @@ int main() {
     int dec;
     bool playing = false;
     bool no_win = false;
+    bool S1 = false;
+    bool S2 = false;
+    bool S3 = false;
+    bool S4 = false;
+    bool S5 = false;
+    bool S6 = false;
+    bool S7 = false;
+    bool S8 = false;
+    bool S9 = false;
+
     cout << "each slot has a number that is assigned to himself" << endl;
     cout << "you're gonna use them to play, they are assigned like that:" << endl;
     for (int i = 0; i < 5; i++){
@@ -25,7 +35,7 @@ int main() {
     no_win = true;
     
     do {
-        playing = true
+        playing = true;
         for (int i = 0; i < 5; i++){
             for (int j = 0; j < 5; j++){
                 cout << Board[i][j] << " ";
@@ -35,9 +45,10 @@ int main() {
         cin >> dec;
         switch(dec){
             case(1):
-                if (Board[0][0] == " 1 ") {
+                if (not S1) {
                     Board[0][0] = " X ";
                     cout << endl;
+                    S1 = true;
                     break;
                 }
                 else
@@ -45,9 +56,10 @@ int main() {
                     cout << endl;
                     break;
             case(2):
-                if (Board[0][2] == " 2 ") {
+                if (not S2) {
                     Board[0][2] = " X ";
                     cout << endl;
+                    S2 = true;
                     break;
                 }
                 else
@@ -55,9 +67,10 @@ int main() {
                     cout << endl;
                     break;
             case(3):
-                if (Board[0][4] == " 3 ") {
+                if (not S3) {
                     Board[0][4] = " X ";
                     cout << endl;
+                    S3 = true;
                     break;
                 }
                 else
@@ -65,9 +78,10 @@ int main() {
                     cout << endl;
                     break;
             case(4):
-                if (Board[2][0] == " 4 ") {
+                if (not S4 ) {
                     Board[2][0] = " X ";
                     cout << endl;
+                    S4 = true;
                     break;
                 }
                 else
@@ -75,9 +89,10 @@ int main() {
                     cout << endl;
                     break;
             case(5):
-                if (Board[2][2] == " 5 ") {
+                if (not S5) {
                     Board[2][2] = " X ";
                     cout << endl;
+                    S5 = true;
                     break;
                 }
                 else
@@ -85,9 +100,10 @@ int main() {
                     cout << endl;
                     break;
             case(6):
-                if (Board[2][4] == " 6 ") {
+                if (not S6) {
                     Board[2][4] = " X ";
                     cout << endl;
+                    S6 = true;
                     break;
                 }
                 else
@@ -95,9 +111,10 @@ int main() {
                     cout << endl;
                     break;
             case(7):
-                if (Board[4][0] == " 7 ") {
+                if (not S7) {
                     Board[4][0] = " X ";
                     cout << endl;
+                    S7 = true;
                     break;
                 }
                 else
@@ -105,9 +122,10 @@ int main() {
                     cout << endl;
                     break;
             case(8):
-                if (Board[4][2] == " 8 ") {
+                if (not S8) {
                     Board[4][2] = " X ";
                     cout << endl;
+                    S8 = true;
                     break;
                 }
                 else
@@ -115,9 +133,10 @@ int main() {
                     cout << endl;
                     break;
             case(9):
-                if (Board[4][4] == " 9 ") {
+                if (not S9) {
                     Board[4][4] = " X ";
                     cout << endl;
+                    S9 = true;
                     break;
                 }
                 else
