3aca1e4edf4243225a24834be14112df2f8008f7
yatomikazuchi <caioevilabner@gmail.com>
2024-05-22 18:14:53 -0300

    updated

--- Pointers.cpp
+++ Pointers.cpp
@@ -9,8 +9,23 @@ int main() {
     // int dinheiros2 = dinheiros; // this creates a copy
     int* dinheiros2 = &dinheiros; // this creates a pointer
 
-    dinheiros2 += 100;
 
+    dinheiros += 12;
+    *dinheiros2 += 100;
+    
     cout << dinheiros << endl;
-    cout << dinheiros2 << endl;
+    cout << *dinheiros2 << endl;
+
+    vector<string> Country = {"Brazil", "USA", "Angola", "Japao"};
+    vector<string>* CountryPtr = &Country;
+
+    (*CountryPtr).push_back("Bolivia");
+
+    for (size_t i = 0; i < Country.size(); i++) {
+        cout << Country[i] << " ";
+    } cout << endl;
+
+    for (size_t i = 0; i < (*CountryPtr).size(); i++) {
+        cout << (*CountryPtr)[i] << " ";
+    } cout << endl;
 }
