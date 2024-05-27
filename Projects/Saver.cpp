c1ed4bdcc13eb7dc8f6d5d820a1c18568d2264a1
yatomikazuchi <caioevilabner@gmail.com>
2024-05-27 19:16:34 -0300

    updated

--- Projects/Saver.cpp
+++ Projects/Saver.cpp
@@ -3,20 +3,29 @@ Implemente funcionalidades para calcular a média das notas e determinar a aprov
 Armazene os dados em um arquivo para persistência de dados.
 */
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-float MediaNota(float nota1, float nota2){
-    float x = (nota1 + nota2) / 2;
-    return x;
-}
-
-int main() {
+float MediaNota(){
     float nota1, nota2;
     cout << "insert the first grade: " << endl;
     cin >> nota1;
     cout << "insert the second grade: " << endl;
     cin >> nota2;
-    float media = MediaNota(nota1, nota2);
+    float x = (nota1 + nota2) / 2;
+    return x;
+}
+
+void NovoCadastro(){
+}
+
+int main() {
+    vector<vector<string>> Alunos = {   
+                                        {"Henry"},
+                                        {"Mary"},
+    };
+    float media = MediaNota();
     cout << media << endl;
 }
