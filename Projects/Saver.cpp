e5cfad4dc9fdd45ca1b870405e7f673c7ed5aa81
yatomikazuchi <caioevilabner@gmail.com>
2024-05-27 19:00:38 -0300

    updated

---
+++ Projects/Saver.cpp
@@ -0,0 +1,19 @@
+/*Crie um sistema para cadastro de estudantes, incluindo informações como nome, idade, matrícula e notas.
+Implemente funcionalidades para calcular a média das notas e determinar a aprovação dos estudantes.
+Armazene os dados em um arquivo para persistência de dados.
+*/
+#include <iostream>
+
+using namespace std;
+
+float MediaNota(float nota1, float nota2){
+
+}
+
+int main() {
+    float nota1, nota2;
+    cout << "insert the first grade: " << endl;
+    cin >> nota1;
+    cout << "insert the second grade: " << endl;
+    cin >> nota2;
+    }
