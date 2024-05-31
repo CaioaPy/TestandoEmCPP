bc9a53154b5df09a5a4f0d56518dd851aa70053c
yatomikazuchi <caioevilabner@gmail.com>
2024-05-30 22:44:43 -0300

    updated

--- Projects/Hangman/Hangman.cpp
+++ Projects/Hangman/Hangman.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <cstring>
 
 using namespace std;
 
@@ -12,19 +13,30 @@ using namespace std;
 int RNG() {
     int x;
     srand(time(0));
-    x = (rand() % 10) + 1;
+    x = (rand() % 3);
     return x;
+}
+
+void errorShow(string boneco[][1]) {
+    for (int i = 0; i < 3; i++){
+        int j = 0;
+        cout << boneco[i][j] << endl;
+    }
 }
 
 int main() {
     string boneco[][1] =   {{""},
                             {""},
                             {""}
-                        };
+                           };
     int x = RNG();
+    string words [4] = {"water", "sugar", "fire", "monster"};
+    string word = words[x];
+    int t = word.length();
+    char* word_array = new char[t + 1];
+    strcpy(word_array, word.c_str());
     cout << x << endl;
-    for (int i = 0; i < 3; i++){
-        int j = 0;
-        cout << boneco[i][j] << endl;
-    }
+    cout << word << endl;
+    cout << word_array[2];
+
 }
